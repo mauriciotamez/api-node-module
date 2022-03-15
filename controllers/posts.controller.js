90418acee7fc640b8e2a03ee3b2cae1e1222b233
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-03-15 17:31:02 -0600

    Run prettier and wrap all of our functions with the handleError fn.

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -10,52 +10,68 @@ const { handleError } = require('../util/handleError')
 
 // Get all posts
 // export const getAllPosts
-exports.getAllPosts = handleError(async (req, res, next) => {
-  // SELECT * FROM posts WHERE status = 'active'; -> posts[]
-  const posts = await Post.findAll({
-    where: { status: 'active' },
-    include: [{ model: User }]
-  })
+exports.getAllPosts = handleError(
+  async (req, res, next) => {
+    // SELECT * FROM posts WHERE status = 'active'; -> posts[]
+    const posts = await Post.findAll({
+      where: { status: 'active' },
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password'] }
+        }
+      ]
+    })
 
-  if (!posts) {
-    return next(new AppError(500, 'Server error'))
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      posts
+    if (!posts) {
+      return next(new AppError(500, 'Server error'))
     }
-  })
-})
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        posts
+      }
+    })
+  }
+)
 
 // Get post by id
-exports.getPostById = handleError(async (req, res, next) => {
-  const { id } = req.params
+exports.getPostById = handleError(
+  async (req, res, next) => {
+    const { id } = req.params
 
-  // SELECT * FROM posts WHERE id = 1;
-  const post = await Post.findOne({
-    where: { id: id, status: 'active' }
-  })
+    // SELECT * FROM posts WHERE id = 1;
+    const post = await Post.findOne({
+      where: { id: id, status: 'active' }
+    })
 
-  if (!post) {
-    return next(new AppError(404, 'No post was found with given ID'))
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      post
+    if (!post) {
+      return next(
+        new AppError(404, 'No post was found with given ID')
+      )
     }
-  })
-})
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        post
+      }
+    })
+  }
+)
 
 // Save post to database
 exports.createPost = handleError(async (req, res, next) => {
   const { title, content, userId } = req.body
 
   if (!title || !content || !userId) {
-    return next(new AppError(400, 'Must provide a title, content and userID'))
+    return next(
+      new AppError(
+        400,
+        'Must provide a title, content and userID'
+      )
+    )
   }
 
   // INSERT INTO posts (title, content, author) VALUES ('A new post', 'Saved in db', 'Max')
@@ -72,8 +88,8 @@ exports.createPost = handleError(async (req, res, next) => {
 })
 
 // Update post (put)
-exports.updatePostPut = async (req, res) => {
-  try {
+exports.updatePostPut = handleError(
+  async (req, res, next) => {
     const { id } = req.params
     const { title, content, author } = req.body
 
@@ -88,7 +104,8 @@ exports.updatePostPut = async (req, res) => {
     ) {
       res.status(400).json({
         status: 'error',
-        message: 'Must provide a title, content and the author for this request'
+        message:
+          'Must provide a title, content and the author for this request'
       })
       return
     }
@@ -113,16 +130,19 @@ exports.updatePostPut = async (req, res) => {
 
     // 204 - No content
     res.status(204).json({ status: 'success' })
-  } catch (error) {
-    console.log(error)
   }
-}
+)
 
 // Update post (patch)
-exports.updatePostPatch = async (req, res) => {
-  try {
+exports.updatePostPatch = handleError(
+  async (req, res, next) => {
     const { id } = req.params
-    const data = filterObj(req.body, 'title', 'content', 'author') // { title } | { title, author } | { content }
+    const data = filterObj(
+      req.body,
+      'title',
+      'content',
+      'author'
+    ) // { title } | { title, author } | { content }
 
     const post = await Post.findOne({
       where: { id: id, status: 'active' }
@@ -139,36 +159,30 @@ exports.updatePostPatch = async (req, res) => {
     await post.update({ ...data }) // .update({ title, author })
 
     res.status(204).json({ status: 'success' })
-  } catch (error) {
-    console.log(error)
   }
-}
+)
 
 // Delete post
-exports.deletePost = async (req, res) => {
-  try {
-    const { id } = req.params
+exports.deletePost = handleError(async (req, res, next) => {
+  const { id } = req.params
 
-    const post = await Post.findOne({
-      where: { id: id, status: 'active' }
+  const post = await Post.findOne({
+    where: { id: id, status: 'active' }
+  })
+
+  if (!post) {
+    res.status(404).json({
+      status: 'error',
+      message: 'Cant delete post, invalid ID'
     })
-
-    if (!post) {
-      res.status(404).json({
-        status: 'error',
-        message: 'Cant delete post, invalid ID'
-      })
-      return
-    }
-
-    // DELETE FROM posts WHERE id = 1;
-    // await post.destroy();
-
-    // Soft delete
-    await post.update({ status: 'deleted' })
-
-    res.status(204).json({ status: 'success' })
-  } catch (error) {
-    console.log(error)
+    return
   }
-}
+
+  // DELETE FROM posts WHERE id = 1;
+  // await post.destroy();
+
+  // Soft delete
+  await post.update({ status: 'deleted' })
+
+  res.status(204).json({ status: 'success' })
+})
